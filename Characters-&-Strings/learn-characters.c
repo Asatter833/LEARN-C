2a0d66e9f0e566743d87fff88f43b6eb733be40a
Asatter833 <asatter833@gmail.com>
2022-12-04 00:57:51 +0600

    Basic Character C programe

---
+++ Characters-&-Strings/learn-characters.c
@@ -0,0 +1,18 @@
+// Basic character
+
+#include <stdio.h>
+#include <ctype.h> //Header file to access functions of characters
+int main()
+{
+    // Declare,input and take a Character
+    char character1 = 'A';
+    char character2;
+    scanf("%c", &character2);
+    printf("character is = %c\n", character2);
+    printf("ASCII Value of the character 1 = %d, \n", character1);
+
+    // islower function checks whether a character is lowercase alphabet or not
+
+    // char lowerCase = 'N';
+    // int islower(lowerCase);
+}
