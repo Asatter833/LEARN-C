3c4a25ac5f136294c8c55977b360b917bd8ed629
Asatter833 <asatter833@gmail.com>
2022-12-07 14:57:06 +0600

    Integer data types

---
+++ Basic/DataTypes/main.c
@@ -0,0 +1,22 @@
+// including header library
+//
+//
+#include <stdio.h> /*Staandard input/out header library of C */
+int main()
+{
+    // Declare Section
+    /*
+    .
+    .
+    .*/
+    // INTEGER VALUE DECLARE
+    int id, age, regular_Num;                  /* Stores atleast 2 bytes(16bits), Usually 4 bytes (32bits) of integer data */
+    short int sinteger;                        /* Stores 2 bytes(8bits) of integer data*/
+    long int long_integer;                     /* Stores atleast 4 bytes(32bits), Usually 8 bytes (64bits) of integer data */
+    long long int;                             /* Stores 8 bytes(64bits) of integer data*/
+    unsigned int positive_integer;             /* Stores Only positive integer numbers*/
+    unsigned long int positive_long_int;       /* Stores atleast 4 bytes(32bits), Usually 8 bytes (64bits) of positive integer data */
+    unsigned long long int positive_long_lint; /* Stores 8 bytes (64bits) of positive integer data */
+
+    // FLOATING VALUE DECLARE
+}
