2b1ac9e8655ae971960a8e9865eab8be8c4bcae3
Asatter833 <asatter833@gmail.com>
2022-12-08 13:22:45 +0600

    Data Type Study-2

--- Basic/DataTypes/main.c
+++ Basic/DataTypes/main.c
@@ -10,13 +10,21 @@ int main()
     .
     .*/
     // INTEGER VALUE DECLARE
-    int id, age, regular_Num;                  /* Stores atleast 2 bytes(16bits), Usually 4 bytes (32bits) of integer data */
-    short int sinteger;                        /* Stores 2 bytes(8bits) of integer data*/
-    long int long_integer;                     /* Stores atleast 4 bytes(32bits), Usually 8 bytes (64bits) of integer data */
-    long long int;                             /* Stores 8 bytes(64bits) of integer data*/
-    unsigned int positive_integer;             /* Stores Only positive integer numbers*/
-    unsigned long int positive_long_int;       /* Stores atleast 4 bytes(32bits), Usually 8 bytes (64bits) of positive integer data */
+    int id = 1212, age = 21, regular_Num;      /* Stores at least 2 bytes(16bits), Usually 4 bytes (32bits) of integer data */
+    short int sinteger;                        /* Stores 2 bytes(8bits) of integer data */
+    long int long_integer;                     /* Stores at least 4 bytes(32bits), Usually 8 bytes (64bits) of integer data */
+    long long int;                             /* Stores 8 bytes(64bits) of integer data */
+    unsigned int positive_integer;             /* Stores Only positive integer numbers */
+    unsigned long int positive_long_int;       /* Stores at least 4 bytes(32bits), Usually 8 bytes (64bits) of positive integer data */
     unsigned long long int positive_long_lint; /* Stores 8 bytes (64bits) of positive integer data */
 
     // FLOATING VALUE DECLARE
+    float salary, decimal_Num = 22.4422; /* Stores 4 bytes(32bits) of decimal floating (.) data */
+    double price;                        /* Stores 8 bytes(64bits) of decimal floating (.) data */
+    long double Lonng_double_num;        /* Stores at least 10 bytes(80bits), Usually 12 or 16 bytes (96 or 128 bits) of decimal floating (.) data */
+
+    // CHARACTER VALUE DECLARE
+    char test_character = 'U'; /* Stores 1 bytes(8bit) of character data */
+    signed char sCharacter;    /* Stores 1 bytes(7bits of data and 1 bit of sign bit) */
+    unsigned char uCharacters; /* Stores 1 bytes (8bits of data no sign bit)*/
 }
